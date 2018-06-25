f866d521e239ab095f8c9a338025e9df53974e91
Kabin Dulal <dulalkabin99@gmail.com>
2018-06-25 13:10:38 -0400

    First Commit Java Bean

---
+++ src/main/java/com/dulal/lesson5creatingbean/Lesson5creatingbeanApplication.java
@@ -0,0 +1,12 @@
+package com.dulal.lesson5creatingbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson5creatingbeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson5creatingbeanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dulal/lesson5creatingbean/Lesson5creatingbeanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dulal.lesson5creatingbean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lesson5creatingbeanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
